ab31346e1c96ba3917768b37ae1bf45d65b3e2db
kevnflyn <rwestanton@gmail.com>
2022-09-17 16:23:20 +0200

    style: clean up styling

--- src/components/ApplicationWrapper.jsx
+++ src/components/ApplicationWrapper.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { Layout, Space } from 'antd'
-import styles from './ApplicationWrapper.module.css'
 import Brand from './Brand'
+import styles from './ApplicationWrapper.module.css'
 
 const { Header, Content } = Layout
 
 const ApplicationWrapper = ({ children }) => (
   <Layout>
     <Header>
-      <Space className='logo' direction='horizontal'>
+      <Space direction='horizontal'>
         <Brand/>
       </Space>
     </Header>
     <Layout className={styles.layout}>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Content className={styles.content}>
-          {children}
-        </Content>
-      </Layout>
+      <Content className={styles.content}>
+        {children}
+      </Content>
     </Layout>
   </Layout>
 )
