4c9a57af657b678c226cca47d67ee86a4c70b2ca
kevnflyn <rwestanton@gmail.com>
2022-09-17 13:58:57 +0200

    refactor: move fetching code to parent above

--- src/pages/detail/DetailPage.jsx
+++ src/pages/detail/DetailPage.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import ScenariosSankyChart from './ScenariosSankyChart'
 
 import styles from './DetailPage.module.css'
-import { Button, Radio } from 'antd'
+import { Radio } from 'antd'
 import { useCallback } from 'react'
 import { useParams } from 'react-router-dom'
 import BackButton from '../../components/BackButton'
 import { routes } from '../../routes'
+import getSankyChartData from "./getSankyChartData.js"
+import { Typography } from "antd"
 
 const timesOfYear = [
   {
@@ -25,19 +27,49 @@ const timesOfYear = [
 
 const DetailPage = () => {
   const [timeOfYear, setTimeOfYear] = useState('yearValue')
+  const [scenarioData, setScenarioData] = useState(null)
+  const { id } = useParams()
+
   const onChangeRadio = useCallback(({ target }) => {
     setTimeOfYear(target.value)
   }, [])
-  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchScenario = async () => {
+      try {
+        // const response = await fetch(`http://localhost:8000/scenarios/${id}`)
+        const response = await fetch(`../../../data/scenario_${id}.json`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        const scenario = await response.json()
+        const scenarioChartData = getSankyChartData(scenario, timeOfYear)
+        setScenarioData(scenarioChartData)
+      } catch (error) {
+        console.warn(error)
+      }
+    }
+    fetchScenario()
+  }, [])
+
+  if (scenarioData === null) {
+    return null
+  }
+
   return (
     <div className={styles.container}>
       <BackButton href={routes.home}/>
+      <Typography.Title level={1}>{scenarioData.name}</Typography.Title>
       <Radio.Group value={timeOfYear} onChange={onChangeRadio}>
         {timesOfYear.map(({ value, label }) =>
           <Radio key={value} value={value}>{label}</Radio>
         )}
       </Radio.Group>
-      <ScenariosSankyChart timeOfYear={timeOfYear} id={id}/>
+      <ScenariosSankyChart
+        scenario={scenarioData}
+        timeOfYear={timeOfYear}
+        id={id}/>
     </div>
   )
 }
--- src/pages/detail/ScenariosSankyChart.jsx
+++ src/pages/detail/ScenariosSankyChart.jsx
@@ -1,48 +1,20 @@
-import React, { useEffect, useState } from "react";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import Sankey from "highcharts/modules/sankey";
-import getSankyChartOptions from "./getSankyChartOptions.js";
-import getSankyChartData from "./getSankyChartData.js";
-import { Typography } from "antd";
+import React from "react"
+import Highcharts from "highcharts"
+import HighchartsReact from "highcharts-react-official"
+import Sankey from "highcharts/modules/sankey"
+import getSankyChartOptions from "./getSankyChartOptions.js"
 
-Sankey(Highcharts);
+Sankey(Highcharts)
 
-const ScenariosSankyChart = ({ timeOfYear, id }) => {
-  const [scenarioData, setScenarioData] = useState(null);
-
-  useEffect(() => {
-    const fetchScenario = async () => {
-      try {
-        // const response = await fetch(`http://localhost:8000/scenarios/${id}`)
-        const response = await fetch(`../../../data/scenario_${id}.json`, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const scenario = await response.json();
-        const scenarioChartData = getSankyChartData(scenario, timeOfYear);
-        setScenarioData(scenarioChartData);
-      } catch (error) {
-        console.warn(error);
-      }
-    };
-    fetchScenario();
-  }, []);
-
-  if (scenarioData === null) {
-    return null;
-  }
-
+const ScenariosSankyChart = ({ scenario, timeOfYear }) => {
   return (
     <div key={timeOfYear}>
-      <Typography.Title level={1}>{scenarioData.name}</Typography.Title>
       <HighchartsReact
         highcharts={Highcharts}
-        options={getSankyChartOptions(scenarioData, "Typical Days GWH 2020")}
+        options={getSankyChartOptions(scenario, "Typical Days GWH 2020")}
       />
     </div>
-  );
-};
+  )
+}
 
-export default ScenariosSankyChart;
+export default ScenariosSankyChart
