da54f825b04fee9693901b96acbde34e54fe8a03
kevnflyn <rwestanton@gmail.com>
2022-09-17 15:19:06 +0200

    feat: favicon

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+// import WindmillSvg from '../assets/windmill.svg'
+import { Image, Space } from 'antd'
 
 import styles from './Brand.module.css'
 
 const Brand = () => (
-  <div className={styles.brand}>ENERGY EXPLORER</div>
+  <Space className={styles.brand}>
+    {/* <WindmillSvg/> */}
+    <Image
+      width='100%'
+      height='20px'
+      src='/windmill.svg'
+    />
+    {/* <img src={'public/windmill.svg'}/> */}
+    ENERGY EXPLORER
+  </Space>
 )
 
 export default Brand
