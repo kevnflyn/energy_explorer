b8b16600ea1b867ff3d7853f0d018eafe7fd4219
Stéphane Bisinger <stephane@sbisinger.ch>
2022-09-17 13:54:12 +0200

    Add correct fetch command as a comment

--- src/pages/detail/ScenariosSankyChart.jsx
+++ src/pages/detail/ScenariosSankyChart.jsx
@@ -1,36 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
-import Sankey from 'highcharts/modules/sankey'
-import getSankyChartOptions from './getSankyChartOptions.js'
-import getSankyChartData from './getSankyChartData.js'
-import { Typography } from 'antd'
+import React, { useEffect, useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import Sankey from "highcharts/modules/sankey";
+import getSankyChartOptions from "./getSankyChartOptions.js";
+import getSankyChartData from "./getSankyChartData.js";
+import { Typography } from "antd";
 
-Sankey(Highcharts)
+Sankey(Highcharts);
 
 const ScenariosSankyChart = ({ timeOfYear, id }) => {
-  const [scenarioData, setScenarioData] = useState(null)
+  const [scenarioData, setScenarioData] = useState(null);
 
   useEffect(() => {
     const fetchScenario = async () => {
       try {
+        // const response = await fetch(`http://localhost:8000/scenarios/${id}`)
         const response = await fetch(`../../../data/scenario_${id}.json`, {
           headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        const scenario = await response.json()
-        const scenarioChartData = getSankyChartData(scenario, timeOfYear)
-        setScenarioData(scenarioChartData)
+            "Content-Type": "application/json",
+          },
+        });
+        const scenario = await response.json();
+        const scenarioChartData = getSankyChartData(scenario, timeOfYear);
+        setScenarioData(scenarioChartData);
       } catch (error) {
-        console.warn(error)
+        console.warn(error);
       }
-    }
-    fetchScenario()
-  }, [])
+    };
+    fetchScenario();
+  }, []);
 
   if (scenarioData === null) {
-    return null
+    return null;
   }
 
   return (
@@ -38,13 +39,10 @@ const ScenariosSankyChart = ({ timeOfYear, id }) => {
       <Typography.Title level={1}>{scenarioData.name}</Typography.Title>
       <HighchartsReact
         highcharts={Highcharts}
-        options={getSankyChartOptions(
-          scenarioData,
-          'Typical Days GWH 2020')
-        }
+        options={getSankyChartOptions(scenarioData, "Typical Days GWH 2020")}
       />
     </div>
-  )
-}
+  );
+};
 
-export default ScenariosSankyChart
+export default ScenariosSankyChart;
