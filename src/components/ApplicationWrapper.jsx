7a3897b752691efbd802910e8679da35901c8c69
kevnflyn <rwestanton@gmail.com>
2022-09-17 15:23:05 +0200

    chore: clean up commented code

--- src/components/ApplicationWrapper.jsx
+++ src/components/ApplicationWrapper.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Breadcrumb, Menu, Space } from 'antd'
+import { Layout Space } from 'antd'
 import styles from './ApplicationWrapper.module.css'
 import Brand from './Brand'
 
@@ -8,32 +8,13 @@ const { Header, Content } = Layout
 const ApplicationWrapper = ({ children }) => (
   <Layout>
     <Header>
-      <Space direction='horizontal'>
+      <Space className='logo' direction='horizontal'>
         <Brand/>
       </Space>
-      {/* <div className="logo" /> */}
-      {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} /> */}
     </Header>
     <Layout className={styles.layout}>
-      {/* <Sider width={200}> */}
-        {/* <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-          items={items2}
-        /> */}
-      {/* </Sider> */}
       <Layout style={{ padding: '0 24px 24px' }}>
-        {/* <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb> */}
-        <Content
-          className={styles.content}
-          // className="site-layout-background"
-        >
+        <Content className={styles.content}>
           {children}
         </Content>
       </Layout>
