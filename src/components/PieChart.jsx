5dbf98fdaab785ca55b9dff9a3d14658369224ad
Stéphane Bisinger <stephane@sbisinger.ch>
2022-09-23 15:54:54 +0200

    Add pie chart for imports composition

---
+++ src/components/PieChart.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+export function formatImportsPieChart(scenario, timeOfYear) {
+  const totalEnergy = scenario.data.energySources
+    .map((sourceName) => {
+      if (sourceName.split("|").length != 2 || !sourceName.endsWith("|Total")) {
+        return 0;
+      }
+      const source = scenario.data[sourceName];
+      return source[timeOfYear];
+    })
+    .reduce((a, b) => a + b);
+  const imports = scenario.data.energySources.flatMap((sourceName) => {
+    const importSources = ["Gas", "Oil", "Coal"];
+    const importRegex = new RegExp(
+      "(" + importSources.map((source) => `\\|${source}`).join("|") + ")$"
+    );
+    if (!importRegex.test(sourceName)) {
+      return [];
+    }
+    const source = scenario.data[sourceName];
+    return [{ y: source[timeOfYear], name: sourceName }];
+  });
+  const domestic =
+    totalEnergy -
+    imports.reduce((a, b) => {
+      return { y: a.y + b.y };
+    }).y;
+  return [...imports, { y: domestic, name: "Domestic" }].sort(
+    (a, b) => a.y - b.y
+  );
+}
+const PieChart = ({ scenario }) => {
+  const getChartOptions = () => ({
+    chart: { type: "pie" },
+    title: { text: "" },
+    tooltip: {
+      pointFormat: "<strong>{point.y:.2f} ({point.percentage:.1f}%)</strong>",
+    },
+    series: [
+      {
+        type: "pie",
+        data: scenario,
+      },
+    ],
+  });
+  const [chartOptions, setChartOptions] = useState(getChartOptions());
+  useEffect(() => {
+    setChartOptions(getChartOptions());
+  }, [scenario]);
+  return (
+    <HighchartsReact
+      // constructorType = { 'sankyChart' }
+      allowChartUpdate={true}
+      immutable={false}
+      highcharts={Highcharts}
+      options={chartOptions}
+    />
+  );
+};
+
+export default PieChart;
--- src/pages/detail/DetailPage.jsx
+++ src/pages/detail/DetailPage.jsx
@@ -1,86 +1,97 @@
-import React, { useEffect, useState, useCallback } from "react"
-import { useParams } from 'react-router-dom'
-import { Radio, Space, Typography } from 'antd'
+import React, { useEffect, useState, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { Radio, Space, Typography } from "antd";
 
-import ApplicationWrapper from "../../components/ApplicationWrapper"
-import BackButton from '../../components/BackButton'
-import ScenariosSankyChart from './ScenariosSankyChart'
-import scenarioTitles from '../../scenarioTitleMap.json'
-import getSankyChartData from "./getSankyChartData.js"
-import { routes } from '../../routes'
+import ApplicationWrapper from "../../components/ApplicationWrapper";
+import BackButton from "../../components/BackButton";
+import ScenariosSankyChart from "./ScenariosSankyChart";
+import scenarioTitles from "../../scenarioTitleMap.json";
+import getSankyChartData from "./getSankyChartData.js";
+import { routes } from "../../routes";
 
-import styles from './DetailPage.module.css'
+import styles from "./DetailPage.module.css";
+import PieChart, { formatImportsPieChart } from "../../components/PieChart";
 
 const timesOfYear = [
   {
-    label: 'Winter',
-    value: 'winterValue'
+    label: "Winter",
+    value: "winterValue",
   },
   {
-    label: 'Summer',
-    value: 'summerValue'
+    label: "Summer",
+    value: "summerValue",
   },
   {
-    label: 'Year',
-    value: 'yearValue'
-  }
-]
+    label: "Year",
+    value: "yearValue",
+  },
+];
 
 const DetailPage = () => {
-  const [timeOfYear, setTimeOfYear] = useState('yearValue')
-  const [scenarioData, setScenarioData] = useState(null)
-  const { id } = useParams()
+  const [timeOfYear, setTimeOfYear] = useState("yearValue");
+  const [scenarioData, setScenarioData] = useState(null);
+  const { id } = useParams();
 
   const onChangeRadio = useCallback(({ target }) => {
-    setTimeOfYear(target.value)
-  }, [])
+    setTimeOfYear(target.value);
+  }, []);
 
   useEffect(() => {
     const fetchScenario = async () => {
       try {
         // ToDo: we need to fetch from this API at some point :)
-        const response = await fetch(`http://localhost:8000/scenarios/${id}`)
+        const response = await fetch(`http://localhost:8000/scenarios/${id}`);
         // const response = await fetch(`../../../data/scenario_${id}.json`, {
         //   headers: {
         //     "Content-Type": "application/json",
         //   },
         // })
-        const scenario = await response.json()
-        setScenarioData(scenario)
+        const scenario = await response.json();
+        setScenarioData(scenario);
       } catch (error) {
-        console.warn(error)
+        console.warn(error);
       }
-    }
-    fetchScenario()
-  }, [])
+    };
+    fetchScenario();
+  }, []);
 
   if (scenarioData === null) {
-    return null
+    return null;
   }
 
   return (
     <ApplicationWrapper>
-      <BackButton href={routes.home}/>
-      <br/>
-      <br/>
+      <BackButton href={routes.home} />
+      <br />
+      <br />
       <div className={styles.container}>
-        <Space className={styles.container} direction='vertical' size='large'>
-          <Space direction='vertical' size='middle'>
-            <Typography.Title level={1}>{scenarioTitles[scenarioData.name]} (GWh)</Typography.Title>
+        <Space className={styles.container} direction="vertical" size="large">
+          <Space direction="vertical" size="middle">
+            <Typography.Title level={1}>
+              {scenarioTitles[scenarioData.name]} (GWh)
+            </Typography.Title>
           </Space>
           <Radio.Group value={timeOfYear} onChange={onChangeRadio}>
-            {timesOfYear.map(({ value, label }) =>
-              <Radio key={value} value={value}>{label}</Radio>
-            )}
+            {timesOfYear.map(({ value, label }) => (
+              <Radio.Button size="large" key={value} value={value}>
+                {label}
+              </Radio.Button>
+            ))}
           </Radio.Group>
+          <Typography.Title level={2}>Energy use</Typography.Title>
           <ScenariosSankyChart
             scenario={getSankyChartData(scenarioData, timeOfYear)}
             timeOfYear={timeOfYear}
-            id={id}/>
+            id={id}
+          />
+          <Typography.Title level={2}>Composition of imports</Typography.Title>
+          <PieChart
+            scenario={formatImportsPieChart(scenarioData, timeOfYear)}
+          />
         </Space>
       </div>
     </ApplicationWrapper>
-  )
-}
+  );
+};
 
-export default DetailPage
+export default DetailPage;
